88e2f06b8b9bddc0027304cf9d7db8715bdbc007
Constantinos Argyriou <constarg@pm.me>
2021-11-13 13:16:28 +0200

    Initialize a few things.

---
+++ MIPS-Assembly/small_code/main.s
@@ -0,0 +1,13 @@
+
+
+.data
+
+
+.text global _start
+
+find_max:
+    # This label is a function that calculate the multiplication of an array A and array B and finds
+    # the biggest number that can be produce by the multiplication.
+
+_start:
+
