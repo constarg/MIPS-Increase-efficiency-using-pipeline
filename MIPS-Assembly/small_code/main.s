3b15b58de887123b79d930095dc36074ec818098
Constantinos Argyriou <constarg@pm.me>
2021-11-16 13:37:49 +0200

    Assembly data memeory.

--- MIPS-Assembly/small_code/main.s
+++ MIPS-Assembly/small_code/main.s
@@ -2,8 +2,17 @@
 
 .data
 
-.text 
-.globl _start
+n_size: 16
+.space 8
+array_a: .word 2,-2, 4,-4, 6,-6, 8,-8,-9, 9,-7, 7,-5, 5,-3, 3
+.space 8
+array_b: .word 8, 7, 6, 5, 4, 3, 2, 1,-1,-2,-3,-4,-5,-6,-7,-8
 
-_start:
-    
+.text 
+main:
+    # store the size in the $r0 register.
+    looop_i:
+
+
+
+    halt
